403902b1d3e1669c6cecb4baae238573372502b1
kkiyama117 <k.kiyama117@gmail.com>
2022-04-25 09:47:32 +0900

    Create CMakeLists

--- build.py
+++ build.py
@@ -1,11 +1,14 @@
 import subprocess
 
 if __name__ == "__main__":
-    print("building...")
     folderPath = "build"
     # subprocess.call(['./build.zsh'])
+    print("remove old files")
+    subprocess.call(['rm', '-rf', folderPath])
+    print("building...")
     subprocess.call(['mkdir', '-p', folderPath])
     subprocess.call(['cmake', '..', '-DCMAKE_BUILD_TYPE=Debug '], cwd=folderPath)
-    subprocess.call(['make'],cwd=folderPath)
-    subprocess.call(['./main'],cwd=folderPath)
+    subprocess.call(['make'], cwd=folderPath)
+    print("running...")
+    subprocess.call(['./bin/main'], cwd=folderPath)
     print("end")
--- build.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/zsh
-
-gfortran ./1/test.f90 >  a.out
