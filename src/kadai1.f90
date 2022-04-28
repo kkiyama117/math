69fcf1eaa55d38ea64d081c1907ba6486ee6940a
kkiyama117 <k.kiyama117@gmail.com>
2022-04-28 18:11:42 +0900

    Kadai1

--- src/kadai1.f90
+++ src/kadai1.f90
@@ -1,14 +1,25 @@
 ! Created by kiyama on 4/26/22.
 
 program kadai1
-    integer :: I, J, K(1000) = 1
+    ! Initialize variables
+    integer :: I, J, K(1000) = 0, L = 1
 
     do I = 2, 1000
         if(K(I) == 0) then
-            WRITE(6, *) I
             do J = 2 * I, 1000, I
                 K(J) = 1
             end do
+            K(I) = I
+        end if
+    end do
+    do I = 2, 1000
+        if(K(I) > 1) then
+            if(mod(L, 5)==0) then
+                write(*, '(i6)') K(I)
+            else
+                write(*, '(i6)', advance = 'no') K(I)
+            end if
+            L = L + 1
         end if
     end do
 end program kadai1
