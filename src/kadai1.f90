1bd97def3e26eeefda0beca1c7994fa5e5b5f253
kkiyama117 <k.kiyama117@gmail.com>
2022-04-28 18:21:22 +0900

    Fix kadai1

--- src/kadai1.f90
+++ src/kadai1.f90
@@ -2,16 +2,22 @@
 
 program kadai1
     ! Initialize variables
-    integer :: I, J, K(1000) = 0, L = 1
+    integer :: I, J, K(1000) = 0, L = 1, COUNT = 0
 
     do I = 2, 1000
-        if(K(I) == 0) then
+        ! Check Count is less than 100 and K(I) is not checked
+        if(K(I) == 0.and.COUNT<100) then
+        ! if(K(I) == 0 ) then
             do J = 2 * I, 1000, I
+                ! Remove multiples
                 K(J) = 1
             end do
+            ! Finallize
+            COUNT = COUNT + 1
             K(I) = I
         end if
     end do
+    ! Show result
     do I = 2, 1000
         if(K(I) > 1) then
             if(mod(L, 5)==0) then
