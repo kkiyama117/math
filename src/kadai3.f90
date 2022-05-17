82c121616e4a88a7657d3862a277f603fbea5e3b
kkiyama117 <k.kiyama117@gmail.com>
2022-05-17 10:16:21 +0900

    Keep

--- src/kadai3.f90
+++ src/kadai3.f90
@@ -16,9 +16,24 @@ contains
     end function qsort
 end module m_sort
 
+
+module m_hensa
+    implicit none
+contains
+    pure function hensa(a, b) result(result)
+        real, allocatable :: r(:)
+        real :: result
+        real, intent(in) :: a(:)
+        real, intent(in) :: b
+        real :: k
+
+    end function hensa
+end module m_hensa
+
 program kadai3
     ! module
     use m_sort
+    use m_hensa
     implicit none
     ! function
     ! tmp
@@ -27,11 +42,19 @@ program kadai3
             1.63, -2.85, 7.89, -5.96, 8.23, 8.76, 2.97, 4.57, 5.21, 9.43, &
             3.12, 6.52, -1.58], RESULT
     integer :: i
+    real :: ave
     ! run
+    ave = sum(A) / real(30)
+    print '(A,f6.2)', '平均値:', ave
+    !do-loopで回すと手計算できるがquicksortとやることが変わらず,qsort(a)[0]を取り出すだけなので割愛.
+    print '(A,f6.2)', '最小値:', minval(A)
+    print '(A,f6.2)', '標準偏差:', hensa(A,ave)
+
+    RESULT = qsort(a)
     !write (*, '(f5.2)') A
 
     ! 書式整理
-    RESULT = qsort(a)
+    write (*, *) "ソート結果(quick sort)"
     do I = 1, 30
         write (*, '(f6.2)', advance = 'no') RESULT(i)
     end do
