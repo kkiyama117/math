a46a6423ef8bde701d79c3a0a29d59c70c69b86f
kkiyama117 <k.kiyama117@gmail.com>
2022-05-24 09:11:37 +0900

    Update

--- build.py
+++ build.py
@@ -11,4 +11,15 @@ if __name__ == "__main__":
     subprocess.call(['make'], cwd=folderPath)
     print("running...")
     subprocess.call(['./bin/main'], cwd=folderPath)
+    output_file = 'results/kadai.txt'
+    run_out_file2 = 'results/kadai_run.txt'
+    with open(run_out_file2, 'w') as f:
+        subprocess.Popen(['./build/bin/kadai3'], stdout=f, stderr=f)
+    with open(output_file, 'w') as f:
+        f.write('出力(pythonで取得)\n')
+        f.write('------------------------------\n')
+        with open(run_out_file2, 'r') as f2:
+            for li in f2.readlines():
+                f.write(li)
+        f.write('------------------------------\n')
     print("end")
