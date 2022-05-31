c12e2e32d49b34fed32cd9e1c5b758d5ed0a69b5
kkiyama117 <k.kiyama117@gmail.com>
2022-05-31 11:53:04 +0900

    Update

--- src/kadai4.f90
+++ src/kadai4.f90
@@ -29,7 +29,7 @@ contains
         do
             cnt = cnt + 1
             x = x - f(x) / df(x)
-            if(abs(f(x))<eps) then
+            if(abs(f(x)) < eps) then
                 exit
             end if
         end do
@@ -37,19 +37,54 @@ contains
     end function newton
 end module m_newton
 
+module m_nibun
+    use m_base
+    implicit none
+contains
+    function nibun(eps)
+        real, intent(in) :: eps
+        real :: nibun(2)
+        ! tmp
+        real :: xm = 1.5
+        real :: xp = 4.0
+        real :: x
+        ! 比較用
+        integer :: cnt = 0
+        do
+            cnt = cnt + 1
+            x = (xm + xp) / 2.0
+            if(f(x) < 0.0) then
+                xm = x
+            else
+                xp = x
+            end if
+            if(abs(f(x)) < eps) then
+                exit
+            end if
+        end do
+        nibun = [real(cnt), x]
+    end function nibun
+end module m_nibun
+
 program kadai4
     ! module
     use m_newton
+    use m_nibun
     use m_base
     implicit none
     ! function
     ! tmp
     real :: eps;
-    real :: r_newton(2);
+    real :: r_newton(2), r_nibun(2);
 
     ! run
     eps = 1.0E-6
     print '("基準値: ", e7.1)', eps
     r_newton = newton(eps)
     print '("回数: ",I2 ," 近似値: " f11.9)', nint(r_newton(1)), r_newton(2)
+    print *, abs(f(r_newton(2)))
+
+    r_nibun = nibun(eps)
+    print '("回数: ",I2 ," 近似値: " f11.9)', nint(r_nibun(1)), r_nibun(2)
+    print *, abs(f(r_nibun(2)))
 end program kadai4
