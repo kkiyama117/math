787735cac6ab3dd17142c76acc4fa1f7b49301fb
kkiyama117 <k.kiyama117@gmail.com>
2022-06-04 18:07:14 +0900

    Comp

--- src/kadai5.f90
+++ src/kadai5.f90
@@ -23,7 +23,6 @@ program kadai5
     ! tmp
     real :: a = 0.0, b = 1.0;
     ! run
-    print *, f(a), f(b)
     print *, simpton(f, a, b, 10)
     print *, simpton(f, a, b, 20)
     print *, simpton(f, a, b, 30)
@@ -31,31 +30,36 @@ program kadai5
     print *, simpton(f, a, b, 50)
 
 contains
-    real function simpton(f, a, b, m)
+    real function simpton(f, a, b, n)
         ! 仮引数の型(関数)
         interface
             real function f(x)
                 real, intent(in) :: x
             end function
         end interface
+
         ! tmp
         real :: a, b ;
         real :: h
         real :: tmp;
-        integer :: m, i
+        integer :: n, i ! n=2m
+
         ! run
-        h = (b - a) / m
+        h = (b - a) / n
+        ! f(a)=f(y_0)
         tmp = f(a)
-        do i = 1, m - 1
-            tmp = tmp + i
+        do i = 1, n - 1
             if (mod(i, 2)==0) then
+                ! f(y_n)=f(y_2k)
                 ! 偶数
                 tmp = tmp + 2 * f(a + real(i) * h)
             else
                 ! 奇数
+                ! f(y_n)=f(y_2k-1)
                 tmp = tmp + 4 * f(a + real(i) * h)
             end if
         end do
+        ! f(y_n)=f(y_2m)=f(b)
         tmp = tmp + f(b)
         simpton = tmp * h / 3
     end function simpton
