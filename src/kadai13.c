493c651ca27e1cd3ae2ec3bafb1a196d7177fb14
kkiyama117 <k.kiyama117@gmail.com>
2022-07-29 15:28:51 +0900

    Update

--- src/kadai13.c
+++ src/kadai13.c
@@ -31,10 +31,10 @@ int main(int argc, char *args[]) {
     double t_n = 6.30;
     double p_0 = 0.;
     double q_0 = 1.;
-//    double eps = 0.00001;
     int n = 630;
     Result result = q1(t_0, t_n, n, p_0, q_0);
 
+    printf("630ステップ時のp,q\n");
     printf("p=%f,q=%f\n", result.p, result.q);
 
     return 0;
@@ -43,7 +43,7 @@ int main(int argc, char *args[]) {
 
 Result q1(double t_0, double t_n, int n, double p_0, double q_0) {
     int i;
-    double p, q, t, h, H;
+    double p, q, t, h, H, delta_p, delta_q;
 //    double dp, dq;
     p = p_0;
     q = q_0;
@@ -51,20 +51,23 @@ Result q1(double t_0, double t_n, int n, double p_0, double q_0) {
     h = (t_n - t_0) / (double) n;
 
     // 漸化式を計算
-    printf("t       p       q        H\n");
+    printf("h=0.01, 630ステップで実行(10ステップ毎に出力)");
+    printf("t       p       q        H        δ(real q) δ(real p)\n");
     printf("%5.5lf, %5.5lf, %5.5lf, %5.5lf\n", t, p, q, H);
     for (i = 0; i < n; i++) {
         Result r;
         // Euler法なら
-//        dp = -q;
-//        dq = p;
-//        r.p = p + h * dp;
-//        r.q = q + h * dq;
+        // dp = -q;
+        // dq = p;
+        // r.p = p + h * dp;
+        // r.q = q + h * dq;
         r.q = (1.0 - 0.5 * pow(h, 2.0) + (1 / 24) * pow(h, 4.0)) * q + (h - (1 / 6) * pow(h, 3.0)) * p;
         r.p = (1.0 - 0.5 * pow(h, 2.0) + (1 / 24) * pow(h, 4.0)) * p - (h - (1 / 6) * pow(h, 3.0)) * q;
-        t += h;
         r.H = 0.5 * (pow(r.p, 2.) + pow(r.q, 2.));
-        printf("%5.5lf, %5.5lf, %5.5lf, %5.5lf\n", t, r.p, r.q, r.H);
+        t += h;
+        delta_p = r.p - (-sin(t));
+        delta_q = r.q - cos(t);
+        printf("%5.5lf, %5.5lf, %5.5lf, %5.5lf, %5.5lf, %5.5lf\n", t, r.p, r.q, r.H, delta_p, delta_q);
         p = r.p;
         q = r.q;
         H = r.H;
@@ -72,26 +75,3 @@ Result q1(double t_0, double t_n, int n, double p_0, double q_0) {
     Result r = {p, q, H};
     return r;
 }
-
-//
-//double runge(double t_0, double t_n, int n, double y_0, BaseFunc f) {
-//int i;
-//double y, t, h, k, k1, k2, k3, k4;
-//y = y_0;
-//t = t_0;
-//h = (t_n - t_0) / n;
-//
-//// 漸化式を計算
-//printf("x       y       y_true\n");
-//for (i = 1; i <= n; i++) {
-//k1 = h * f(t, y);
-//k2 = h * f(t + h / 2., y + k1 / 2.);
-//k3 = h * f(t + h / 2., y + k2 / 2.);
-//k4 = h * f(t + h, y + k3);
-//k = (k1 + 2. * k2 + 2. * k3 + k4) / 6.;
-//y += k;
-//t += h;
-//printf("%5.5lf, %5.5lf, %5.5lf\n", t, y, y - y_true(t));
-//}
-//return y;
-//}
