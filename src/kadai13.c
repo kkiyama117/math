59e256cc12ae4109676679f60f5c4a05c4a53608
kkiyama117 <k.kiyama117@gmail.com>
2022-07-29 12:47:58 +0900

    Add 13

---
+++ src/kadai13.c
@@ -0,0 +1,19 @@
+//
+// Created by kiyama on 7/12/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef FALSE
+#define FALSE (0)
+#endif
+
+#ifndef TRUE
+#define TRUE (!FALSE)
+#endif
+
+int main(int argc, char *args[]){
+
+}
